7060551b097f25ad05eaadc36ce3efe67964e6cd
Akash-Patil-12 <patilakash121212@gmail.com>
2021-08-13 20:53:43 +0530

    [Akash] Add : Implemented Message analyse and respond happy or sad mood

---
+++ MoodAnalyser/MoodAnalyser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyser
+{
+    class MoodAnalyser
+    {
+        public string AnalyserMood(string message)
+        {
+            if (message.Contains("happy") || message.Contains("Happy" ))
+            {
+                return "happy";
+            }
+            return "sad";
+        }
+    }
+}
---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Console.WriteLine(moodAnalyser.AnalyserMood("I am sad"));
+        }
+    }
+}
